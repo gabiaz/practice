6398e12f944cbc6e1c695cdf4412af9267cdd8c7
Gabrielle Azevedo <78125543+gabiaz@users.noreply.github.com>
2021-01-28 17:10:01 -0300

    Delete ex057.py

--- ex057.py
+++
@@ -1,4 +0,0 @@
-n = str(input('Qual o seu sexo (F/M)? ')).strip().upper()[0]
-while n not in 'FfMm':
-    print('Digite F para feminino e M para masculino')
-    n = str(input('Digite novamente o sexo: '))
