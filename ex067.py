7846b5f03359c276c5f9f0f1ef7216e4db086481
Gabrielle Azevedo <78125543+gabiaz@users.noreply.github.com>
2023-01-18 23:03:57 -0300

    Delete ex067.py

--- ex067.py
+++
@@ -1,9 +0,0 @@
-c = 0
-while True:
-    n = int(input('Digite um número inteiro: '))
-    if n < 0 :
-        break
-    while c < 10:
-        c = c + 1
-        print (f'{n} X {c} = {n*c}')
-print('Programa encerrado')
