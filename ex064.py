9048f569d2f836706d8b15f0fc31abe7e14fdaa5
Gabrielle Azevedo <78125543+gabiaz@users.noreply.github.com>
2023-01-18 23:01:07 -0300

    Delete ex064.py

--- ex064.py
+++
@@ -1,9 +0,0 @@
-n = 0
-s = 0
-cont = 0
-while n != 999:
-    n = int(input('Digite um número inteiro [999 para parar]: '))
-    s = s + n
-    cont = cont + 1
-print('Você digitou {} números e a soma deles é {} .'.format(cont - 1, s - 999))
-
